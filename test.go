2a286b4ae58df6cfe5470a905a4b67998d56dfbd
vfa-khuongdv <khuongdv@vitalify.asia>
2025-06-03 15:02:29 +0700

    add: unittest

--- test.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/vfa-khuongdv/golang-cms/internal/utils"
-
-func main() {
-	password := utils.HashPassword("123456789")
-	print(password) // Output: hashed password
-}
