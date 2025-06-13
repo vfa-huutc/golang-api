e9d378e09e6fb1b575cf7f81f023b017aeafa0f8
vfa-khuongdv <khuongdv@vitalify.asia>
2025-06-13 08:39:07 +0700

    add: unittest for validation

--- internal/database/migrations/000001_create_users_table.up.sql
+++ internal/database/migrations/000001_create_users_table.up.sql
@@ -1,8 +1,5 @@
 CREATE TABLE `users` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `created_at` datetime(3) DEFAULT NULL,
-  `updated_at` datetime(3) DEFAULT NULL,
-  `deleted_at` datetime(3) DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `birthday` date DEFAULT NULL,
@@ -11,6 +8,9 @@ CREATE TABLE `users` (
   `token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `expired_at` bigint DEFAULT NULL,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uni_users_email` (`email`),
   UNIQUE KEY `uni_users_token` (`token`),
--- pkg/apperror/validation_error_test.go
+++ pkg/apperror/validation_error_test.go
@@ -1,1 +1,59 @@
 package apperror_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vfa-khuongdv/golang-cms/pkg/apperror"
+)
+
+func TestValidateError(t *testing.T) {
+	// Example test case for the Error method
+	err := apperror.NewValidationError("Validation failed", []apperror.FieldError{
+		{Field: "username", Message: "Username is required"},
+		{Field: "email", Message: "Email is invalid"},
+	})
+
+	expected := "code: 4001, message: Validation failed, fields: [{username Username is required} {email Email is invalid}]"
+
+	assert.Equal(t, expected, err.Error())
+}
+
+func TestValidateWrap(t *testing.T) {
+	// Example test case for the Wrap method
+	err := apperror.NewValidationError("Validation failed", []apperror.FieldError{
+		{Field: "password", Message: "Password is too short"},
+	})
+
+	wrappedErr := err.Wrap(400, 4001, "Wrapped validation error")
+	expected := "code: 4001, message: Wrapped validation error, fields: [{password Password is too short}]"
+
+	assert.NotNil(t, wrappedErr)
+	assert.Equal(t, 4001, wrappedErr.Code)
+	assert.Equal(t, "Wrapped validation error", wrappedErr.Message)
+	assert.Equal(t, 1, len(wrappedErr.Fields))
+	assert.Equal(t, expected, wrappedErr.Error())
+}
+
+func TestValidateNewValidationError(t *testing.T) {
+	// Example test case for the NewValidationError function
+	fieldErrors := []apperror.FieldError{
+		{Field: "email", Message: "Email is required"},
+		{Field: "password", Message: "Password must be at least 6 characters"},
+	}
+
+	err := apperror.NewValidationError("Validation failed", fieldErrors)
+
+	assert.Equal(t, 4001, err.Code)
+	assert.Equal(t, "Validation failed", err.Message)
+	assert.Equal(t, 2, len(err.Fields))
+
+	for _, fieldError := range err.Fields {
+		if fieldError.Field == "email" {
+			assert.Equal(t, "Email is required", fieldError.Message)
+		} else if fieldError.Field == "password" {
+			assert.Equal(t, "Password must be at least 6 characters", fieldError.Message)
+		}
+	}
+
+}
